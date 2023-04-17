b9e0828fb61f78f4776e05606816d74b0923e6f0
moonrise0 <moonriseisnow@gmail.com>
2023-04-17 17:41:49 +0900

    "0417 01"

--- src/main/java/com/kbstar/app/App.java
+++ src/main/java/com/kbstar/app/App.java
@@ -1,2 +1,11 @@
-package com.kbstar.app;public class App {
+package com.kbstar.app;
+
+import com.kbstar.frame.*;
+import com.kbstar.tv.*;
+
+public class App {
+    public static void main(String args[]){
+        TV tv = new STV();
+        tv.turnOn();
+    }
 }
